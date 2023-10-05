2068508d6b35baa75b9204c66ad1abead5100748
Aurieliton <60197776+Aurieliton@users.noreply.github.com>
2023-10-05 19:58:27 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+
+class Cachorro:
+    def __init__(self,nome,raca,peso):
+        self.nome = nome
+        self.raca=raca
+        self.peso=peso
+
+
+    def comerRacao(self):
+        print(f'--Informação do Cachorro--')
+        print(f'Nome: {self.nome}')
+        print(f'Raça: {self.raca}')
+        print(f'Peso: {self.peso}')
+        return "croc, croc, croc"
+
+
+cachorro1 = Cachorro('Arlequina','PitDalmata','20')
+cachorro2 = Cachorro('Cintia','loba','10')
+
+cachorro1.comerRacao()
+cachorro2.comerRacao()
